f116599cebb083f08fa092b14579d0915a4967a9
Venceslao87 <markhobrod_v_g@ukr.net>
2026-09-03 02:36:03 +0300

    Add project files.

---
+++ Intro_to_Method/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Title = "Introduce the Method";
+Console.WriteLine("Hello, World!");
+CountToTen();
+void CountToTen()
+{
+    int sum = 0;
+    for (int index = 1; index <= 10; index++)
+    {
+        sum += index;
+        Console.WriteLine(index);
+    }
+    Console.WriteLine($"The sum of numbers is {sum}");
+}
---
+++ Own_variable/Program.cs
@@ -0,0 +1,20 @@
+﻿//Understanding of the own variable in Methods in C#
+Console.Title = "Own Variable in Methods";
+//Intro general variable
+Console.WriteLine("Input the general variable");
+int current = int.Parse(Console.ReadLine());
+// Call the methods
+CountToTen();
+CountToTwenty();
+
+// The space of description of the methods
+void CountToTen()
+{
+    for (int current = 1 /* the own variables of method */; current <= 10; current++)
+        Console.WriteLine(current);
+}
+void CountToTwenty()
+{
+    for (int current = 1 /* the own variables of method */; current <= 20; current++)
+        Console.WriteLine(current);
+}
