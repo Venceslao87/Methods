54a60015f977fec3ea6a56445b7cb49a1aa6c4fa
Venceslao87 <markhobrod_v_g@ukr.net>
2026-09-05 02:06:40 +0300

    add "Parametr" Project

---
+++ Parametrs/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Title = "Working with parameters";
+// Input quantity
+Console.Write("Input the quantity of numbers: ");
+int input = int.Parse(Console.ReadLine());
+//Call the method
+Count(input);
+
+// Description method
+void Count(int current)
+{
+    for (int index = 0; index <= current; index++)
+        Console.WriteLine(index);
+}
